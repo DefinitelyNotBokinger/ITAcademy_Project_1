bff18d02c0e41f8f217a4638800bedc53f06ec44
Bokinger <kurhanovich.n@gmail.com>
2024-04-30 15:14:23 +0300

    Project_1.Dev-starts

---
+++ Project_1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project_1
+//
+//  Created by Nikita Kurhanovich on 22.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var scoreButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func scoreButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
+
